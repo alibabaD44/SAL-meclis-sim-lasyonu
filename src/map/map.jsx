e45edd8f3308fbfbd3d34a2598c6d2ef221fa892
alibabaD44 <yigitoluk145@gmail.com>
2026-02-24 11:35:50 +0300

    bug fixed

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -14,7 +14,7 @@ function Map() {
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d651.4340806120468!2d28.93577601237976!3d41.01334180117348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caba30ce15cf8d%3A0x8f65699445805681!2s%C5%9Eehremini%20Anadolu%20Lisesi!5e0!3m2!1str!2str!4v1771856094329!5m2!1str!2str"
                          loading="lazy" 
-                         referrerpolicy="no-referrer-when-downgrade">
+                         referrerPolicy="no-referrer-when-downgrade">
                     </iframe>
                 </div>
             </section>
