7eb9b9c50957e01ac38c0023fad31a832d5034a4
alibabaD44 <yigitoluk145@gmail.com>
2026-02-25 18:02:28 +0300

    Footer designed

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,13 +1,31 @@
 import './footer.css'
 
-function Footer(){
-    return(
+function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
         <footer className="footer">
-            <div className='footer-bsk'>
-                © 2025 ŞAL Meclis Simülasyonu IT Ekibi
-                <a href="https://www.instagram.com/sehreminimeclis26/" target="_blank" rel="noreferrer" className="footer-insta">
-                    <i className="fab fa-instagram"></i>
-                </a>
+            <div className="footer-container">
+                <div className='footer-logo'>
+                    <img src="/meclislogo.png" alt="Meclis Logo" />
+                </div>
+
+                <div className='footer-info'>
+                    <h2 className='footer-bsk'>ŞAL MECLİS SİMÜLASYONU</h2>
+                </div>
+
+                <div className='footer-links'>
+                    <a href="https://www.instagram.com/sehreminimeclis26/" 
+                       target="_blank" 
+                       rel="noreferrer" 
+                       className="footer-insta">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                </div>
+
+                <div className="footer-copyright">
+                    <p>&copy; {currentYear} Şehremini Anadolu Lisesi. Tüm hakları saklıdır.</p>
+                </div>
             </div>
         </footer>
     )
